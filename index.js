f82233b9d058c06a69b7289beb8b9fa8a0865021
davidotieno <davidotieno473@gmail.com>
2023-09-29 07:52:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' );
+function writeCards(name, event) {
+   
+    const array = []
+    for (let i = 0; i < name.length; i++) {
+        array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    console.log(array);
+    return array;
+
+}
+function countDown(number) {
+    while (number>0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
